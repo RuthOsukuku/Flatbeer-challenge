bbd4be1e1d74f22bb6bd72c4d61134342cb27dbc
Ruth <osukukuruth@gmail.com>
2023-04-18 16:13:04 -0400

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,55 +1,150 @@
 // Code here
-const apiUrl="http://localhost:3000/beers"
-let beerList
-const beerName=document.getElementById("beer-name")
-const beerImage=document.getElementById("beer-image")
-const beerDescription=document.getElementById("beer-description")
-const beerReviews=document.getElementById("review-list")
-const beerNameList=document.getElementById("beer-list")
-function showBeerDetails(beerData){
-    beerName.innerText=beerData.name
-    console.log(beerData.image_url)
-    beerImage.src =beerData.image_url
-    beerDescription.innerText=beerData.description
-    beerReviews.innerText=""
+"use strict"; 
+
+
+const url = 'http://localhost:3000/beers'
+
+const beerName = document.getElementById('beer-name');
+const beerImage = document.getElementById('beer-image');
+const beerDescription = document.getElementById('beer-description');
+const beerReviews = document.getElementById('review-list');
+const beerNames = document.getElementById('beer-list');
+const reviewForm = document.getElementById('review-form');
+const reviewFormTextArea = document.getElementById('review');
+const descriptionForm = document.getElementById('description-form');
+const descriptionFormTextArea = document.getElementById('description');
+let selectedBeer;
+let selectedBeerId;
+
+let beersList;
+
+const showBeerDetails = (data) => {
+    selectedBeerId = data.id;
+    selectedBeer = data;
+    beerName.innerText = data.name;
+    beerImage.src = data.image_url;
+    beerDescription.innerText = data.description;
+    beerReviews.innerHTML = '';
+    beerNames.innerHTML = '';
+    return;
 }
-function showBeerReviews(beerData){
-    for(item of beerData.reviews){
-        const beerReviewList=document.createElement("li")
-        beerReviewList.innerText=item
-        beerReviews.appendChild(beerReviewList)
+
+const showBeerReviews = (data) => {
+    data.reviews.forEach(element => {
+        const beerReviewsLi = document.createElement('li');
+        
+        beerReviewsLi.innerText = element
+        beerReviews.appendChild(beerReviewsLi)
+        
+    });
+    return
+}
+
+const showListOfBeers = (data) => {
+    data.forEach(element => {
+        const beerNameLi = document.createElement('li');
+        beerNameLi.innerText = element.name;
+        beerNames.appendChild(beerNameLi);  
+    });
+    return
+}
+
+function updateRecord(id, updatedData) {
+    fetch(`${url}/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(updatedData)
+    })
+    .then((response) => response.json())
+    .then((data) => beersList = data)
+}
+
+// 1 Remove review from page when clicked
+beerReviews.addEventListener('click', handleRemoveReviewClick)
+// Handle Review Remove Click
+function handleRemoveReviewClick(event) {
+    event.preventDefault();
+    event.target.remove();
+    // removeReview()
+
+}
+
+// 2 Nav click populate main page
+beerNames.addEventListener('click', handleNavClicked)
+// Handle Nav Click
+function handleNavClicked(event) {
+    if(event.target && event.target.matches('li')) {
+        let content =  event.target.textContent
+        selectedBeer  = beersList.find(item =>  item.name == content )
+        showBeerDetails(selectedBeer)
+        showBeerReviews(selectedBeer)
+        showListOfBeers(beersList)
     }
 }
-function getFirstBeer(){
-    fetch(`${apiUrl}/1`)
-    .then(response=>response.json())
-    .then(firstBeer=>{
-        showBeerDetails(firstBeer)
-        showBeerReviews(firstBeer)
-    })
-}
-function showBeerNames(beerData){
-    beerNameList.innerText=""
-    for(item of beerData){
-        const beerNameListItem=document.createElement("li")
-        beerNameListItem.innerText=item.name
-        beerNameList.appendChild(beerNameListItem)
+
+/////////////////////////////////////**EXTRA BONUS**////////////////////////
+// 1 Add a review
+reviewFormTextArea.name = 'review';
+reviewForm.addEventListener('submit', handleReviewFormSubmit);
+
+//Handle Review Form submit
+function handleReviewFormSubmit(event) {
+    event.preventDefault();
+    // const beerReviewsLi = document.createElement('li');
+    
+    const formData = new FormData(reviewForm);
+    reviewFormTextArea.value = '';
+
+    const userReview = {
+        'reviews': [
+            ...selectedBeer.reviews,
+            formData.get('review')
+        ]
     }
+    
+    // beerReviews.appendChild(beerReviewsLi);
+    updateRecord(selectedBeerId, userReview)
 }
-function getAllbeers(){
-    fetch(apiUrl)
-    .then(response=>response.json())
-    .then(allBeers=>{
-    showBeerNames(allBeers)
+
+// 2 Add description
+descriptionFormTextArea.name = 'description';
+descriptionForm.addEventListener('submit', handleDescriptionFormSubmit);
+function handleDescriptionFormSubmit(event) {
+    event.preventDefault();
+    
+    const formData = new FormData(descriptionForm);
+
+    const userDescription = {
+        'description': formData.get('description')
+    
+    }
+    
+    // beerReviews.appendChild(beerReviewsLi);
+    updateRecord(selectedBeerId, userDescription)
+    descriptionFormTextArea.value = '';
+
+}
+
+// 3 Remove review
+function removeReview() {
+    fetch(`${url}/${selectedBeerId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        }
     })
+    .then(response => response.json())
+    .then(beer => console.log(beer))
 }
-function getSecondBeer(){
-    fetch(`${apiUrl}/2`)
-    .then(response=>response.json())
-    .then(secondBeer=>{
-        showBeerDetails(secondBeer)
-        showBeerReviews(secondBeer)
-    })
-}
-getFirstBeer()
-getAllbeers()
+
+/// Fetch All beers
+const getAllBeers = () => fetch('http://localhost:3000/beers').then(res => res.json()).then(data => {
+    beersList = data;
+    showBeerDetails(beersList[0]);
+    showBeerReviews(beersList[0]);
+    showListOfBeers(beersList);
+})
+
+getAllBeers()
